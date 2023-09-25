f82033ec4f801f41b1a4add1eb8be3501f889f6c
Vitaliy <vitaliy-eroshin@yandex.ru>
2023-09-25 12:32:52 +0300

    Added seminar 04 leetcode task

---
+++ practice/seminar-04-leetcode/src/solution.hpp
@@ -0,0 +1,32 @@
+/*
+    Leetcode: 232. Implement Queue using Stacks
+
+    Реализуйте очередь через стеки.
+*/
+
+#pragma once
+
+#include <stack>
+
+class Queue {
+public:
+    Queue() {
+
+    }
+    
+    void Push(int /*x*/) {
+        
+    }
+    
+    int Pop() {
+        return 0;
+    }
+    
+    int Peek() {
+        return 0;
+    }
+    
+    bool Empty() {
+        return false;
+    }
+};
