06f20f3f2d93006ceb9966488f5830404888c034
Vitaliy <vitaliy-eroshin@yandex.ru>
2023-10-02 16:58:06 +0300

    Added seminar 05 leetcode practice

---
+++ practice/seminar-05-leetcode/src/solution.hpp
@@ -0,0 +1,17 @@
+/*
+    Leetcode: 658. Find K Closest Elements
+
+    Дан массив arr, числа k и x. Необходимо
+    вернуть k ближайших к x элементов из arr.
+
+    Результат должен быть отсортирован по возрастанию
+*/
+
+#pragma once
+
+#include <vector>
+#include <queue>
+
+std::vector<int> FindClosestElements(std::vector<int>& /*arr*/, int /*k*/, int /*x*/) {
+    return {};
+}
