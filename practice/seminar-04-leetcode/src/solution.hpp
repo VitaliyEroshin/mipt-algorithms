4ceba2103e2f98dd1d815524f2e5664bf41817b5
Vitaliy <vitaliy-eroshin@yandex.ru>
2023-10-28 00:11:09 +0300

    Added solution for seminar-04-leetcode task

--- practice/seminar-04-leetcode/src/solution.hpp
+++ practice/seminar-04-leetcode/src/solution.hpp
@@ -10,23 +10,34 @@
 
 class Queue {
 public:
-    Queue() {
+    void Push(int x) {
+        in_stack_.push(x);
+    }
 
-    }
+    int Pop();
     
-    void Push(int /*x*/) {
-        
-    }
-    
-    int Pop() {
-        return 0;
-    }
-    
-    int Peek() {
-        return 0;
-    }
+    int Peek();
     
     bool Empty() {
-        return false;
+        return out_stack_.empty() && in_stack_.empty();
     }
+private: 
+    std::stack<int> in_stack_;
+    std::stack<int> out_stack_;
 };
+
+int Queue::Pop() {
+    int elem = Peek();
+    out_stack_.pop();
+    return elem;
+}
+
+int Queue::Peek() {
+    if (out_stack_.empty()) {
+        while (!in_stack_.empty()) {
+            out_stack_.push(in_stack_.top());
+            in_stack_.pop();
+        }
+    }
+    return out_stack_.top();
+}
